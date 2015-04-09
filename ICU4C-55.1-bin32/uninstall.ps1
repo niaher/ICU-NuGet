032bccb0fa796e530acb7299bb4a6dd2744731ee
niaher <niaher.crimston@gmail.com>
2015-04-09 22:17:30 +0700

    Bugfix: added uninstall script to the bin package.

---
+++ ICU4C-55.1-bin32/uninstall.ps1
@@ -0,0 +1,18 @@
+param($installPath, $toolsPath, $package, $project)
+
+$solutionDir = $project.FullName | Split-Path -Parent | Split-Path -Parent
+
+$dlls = (
+	"icudt55.dll", 
+	"icuin55.dll",
+	"icuio55.dll", 
+	"icule55.dll",
+	"iculx55.dll", 
+	"icutu55.dll",
+	"icuuc55.dll")
+
+foreach ($dll in $dlls) {
+	$project.ProjectItems.Item("$dll").Remove()
+}
+
+Remove-Item "$solutionDir\ICU4C" -Recurse -Force
